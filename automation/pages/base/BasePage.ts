07e56283dd144886689f754ae1bee8ab4336fd81
NDLoc-vn <loccoc1807@gmail.com>
2026-04-24 09:18:42 +0700

    test: add playwright config and implement BasePage

---
+++ automation/pages/base/BasePage.ts
@@ -0,0 +1,25 @@
+import { Page, Locator } from "@playwright/test";
+
+export class BasePage {
+  protected page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigate(path: string) {
+    await this.page.goto(path);
+  }
+
+  async click(locator: Locator) {
+    await locator.click();
+  }
+
+  async fill(locator: Locator, value: string) {
+    await locator.fill(value);
+  }
+
+  async getText(locator: Locator) {
+    return await locator.textContent();
+  }
+}
